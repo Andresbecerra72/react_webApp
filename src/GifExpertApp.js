43b650a4cc118f1882943b7a297ef0b7290c1ba7
Andres Becerra <andresbecerra7200@gmail.com>
2021-08-24 17:48:40 -0500

    Initial Commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,18 @@
+
+
+// snipped para crear el componente "rafc"
+
+import React from 'react'
+
+
+
+export const GifExpertApp = () => {
+    return ( // <> fragment
+        <> 
+
+            <h1>GiftExpertApp</h1>
+            <hr />
+            
+        </>
+    )
+}
