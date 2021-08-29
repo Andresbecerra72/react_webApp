a54cb7505a02f887109c15febdf38a6e926a1527
ANDRES BECERRA <31633615+Andresbecerra72@users.noreply.github.com>
2021-08-29 16:38:36 -0500

    Updated Last

--- src/helpers/GetGifs.js
+++ src/helpers/GetGifs.js
@@ -10,7 +10,7 @@ export const getGift = async(category) => {
 
     // encodeURI() es una funcion que escapa los espacios
 
-    const url = `http://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ClvBt8X8PmQoUrP9vO4mmjTPUMDQ0Htm`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=ClvBt8X8PmQoUrP9vO4mmjTPUMDQ0Htm`;
     const resp = await fetch( url );
     const { data } = await resp.json(); // se usa la desestructuracion para recibir solo la data
 
